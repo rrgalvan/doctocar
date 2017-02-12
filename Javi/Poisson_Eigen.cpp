143f20c83e71dbcde604ef636bd74407fa19d07b
Javier Navarro <javi.navarroiz@hotmail.es>
2017-02-13 00:15:40 +0100

    Problema de Poisson con libreria Eigen

--- Javi/Poisson_Eigen.cpp
+++ Javi/Poisson_Eigen.cpp
@@ -11,13 +11,16 @@ float f(float x,float y)
 
 MatrixXf Poisson2D (float (*f)(float,float),float (*g)(float,float), int M)
 {
-	// Definimos las variables
-	float h;
+	// Definimos los valores constantes
+	float h, h2;
+
+	// Definimos las matrices
 	MatrixXf v = MatrixXf::Constant(M+2,M+2,0);
 	MatrixXf f_val(M,M);
 
-	// Calculamos h
-	h = 1/(M+1);
+	// Calculamos h y h2
+	h  = 1/(M+1);
+	h2 = h*h;
 	
     	// Cargamos las condiciones de frontera horizontales.
 	for(int j=0;j<M+2;j++)
@@ -32,17 +35,16 @@ MatrixXf Poisson2D (float (*f)(float,float),float (*g)(float,float), int M)
         	v(i+1,0)   = g((i+1)*h,0);
         	v(i+1,M+1) = g((i+1)*h,1);
 	}
-
     	
 	// Almacenamos los valores de f
 	for(int i=0; i<M; i++)
 		for(int j=0; j<M; j++)
 			f_val(i,j) = f((i+1)*h,(j+1)*h);
 	
-	// Rellenamos el resto de la matriz.
+	// Rellenamos el resto de la matriz
 	for(int k=0;k<M;k++) 
 		v.block(1,1,M,M) = 0.25 * (v.block(0,1,M,M) + v.block(2,1,M,M)
-			+ v.block(1,0,M,M) + v.block(1,2,M,M) + h*h*f_val ); 
+			+ v.block(1,0,M,M) + v.block(1,2,M,M) + h2*f_val ); 
 		
 	return(v);
 }
